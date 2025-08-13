e333fd3ef958987c5fcf1f548b71e69cf4736058
Raul Santamaria <raulsantamariao@hotmail.com>
2025-08-12 21:01:56 -0500

    Enhance ICamera interface: add resolution and format parameters to __init__ method

--- src/sc_be_contracts/interfaces/capturer/camera.py
+++ src/sc_be_contracts/interfaces/capturer/camera.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
 
+from src.sc_be_contracts.models.capturer.format import CaptureFormat
 from src.sc_be_contracts.models.capturer.frame import Frame
 
 
 class ICamera(ABC):
 	@abstractmethod
-	def __init__(self, camera_index: int) -> None: ...
+	def __init__(self, camera_index: int, resolution:tuple[int,int], format: CaptureFormat) -> None: ...
 
 	@abstractmethod
 	def start(self) -> None: ...
